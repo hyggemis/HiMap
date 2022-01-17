6b0066ae90277365b7a29b9f62b901415198ee90
Nguyen Hoang Phuong <nguyenhoangphuong>
2022-01-17 19:15:43 +0700

    update

---
+++ busmap/FindWay/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FindwayConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FindWay'
---
+++ busmap/FindWay/urls.py
@@ -0,0 +1,6 @@
+from django.urls import include, path
+from .views import index_view
+
+urlpatterns = [
+    path('', index_view, name="index"),
+] 
