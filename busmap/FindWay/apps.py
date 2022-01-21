acdeefd4aee971c0852db9e5e9874a3b21d73121
hyggemis <98154060+hyggemis@users.noreply.github.com>
2022-01-21 21:32:09 +0700

    Delete busmap directory

--- busmap/FindWay/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FindwayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FindWay'
--- busmap/FindWay/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from .views import index_view, get_data
-
-urlpatterns = [
-    path('', index_view, name="index"),
-    path('way/', get_data, name = "get_data")
-]
